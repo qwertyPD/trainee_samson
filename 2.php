6093de6362fd9a9f52de0c8d345dace6ea881d7b
Paul <paul.dmn.h@gmail.com>
2022-01-24 11:32:20 +0300

    Added function importXml($a)

--- 2.php
+++ 2.php
@@ -36,4 +36,64 @@ function mySortForKey($a, $b) {
     return $a;
 }
 
+/* Реализовать функцию importXml($a). $a – путь к xml файлу.
+Результат ее выполнения: прочитать файл $a и импортировать его в созданную БД. */
+
+
+function importXml($a) {
+
+    $conn = mysqli_connect("localhost", "root", "", "test_samson");
+    $xml = simplexml_load_file($a) or die ("Невозможно загрзить xml");
+    $categories = array(); // Список добавленных в базу данных категорий
+    $category_id = 0; // ID последней добавленной в базу данных категории
+
+    $sqlLoad = function($table, $column, $data, $conn, $return_id = FALSE) { // $return_id = TRUE если нужен ID добавленной в базу данных категории
+        if (is_array($data)) {
+            $data = implode('\', \'', $data);
+            $column = implode(', ', $column);
+        }
+
+        $sqlRequest = "INSERT INTO test_samson.$table ($column) VALUES ('$data')";
+
+        if ($conn->query($sqlRequest) === TRUE) {
+            $category_id = $conn->insert_id;
+            if ($return_id === TRUE) {
+                return $category_id;
+            }
+        } else {
+            echo "Ошибка: " . $conn->error;
+        }
+    };
+
+    foreach ($xml as $tag) {    
+        $category = $tag->Разделы->children();
+
+        foreach ($category as $item) {
+            $value = (string) $item;
+
+            if (!in_array($item, $categories)) {    //Загрузка категорий
+                $category_id = $sqlLoad('a_category','name', $value, $conn, TRUE);
+                $categories += array($category_id => $value);
+                //Загрузка товаров
+                $values = array($category_id, (integer) $tag['Код'], (string) $tag['Название']);
+                $category_id = $sqlLoad('a_product', array('id', 'code', 'name'), $values, $conn, TRUE);  
+            } else {
+                $values = array(array_search($value, $categories), (integer) $tag['Код'], (string) $tag['Название']);
+                $category_id = $sqlLoad('a_product', array('id', 'code', 'name'), $values, $conn, TRUE); 
+            }
+        }
+        foreach ($tag->Цена as $price) {    //Загрузка цены
+            $values = array((float) $price, (string) $price['Тип'], $tag['Название']);
+            $sqlLoad('a_price', array('price', 'price_type', 'product'), $values, $conn);
+        } 
+        foreach ($tag->Свойства as $properties) {   //Загрузка свойств
+            foreach ($properties as $property) {
+                $values = array($tag['Название'], $property->getName(). ' ' . $property . $property->attributes());
+                $sqlLoad('a_property', array('product', 'property'), $values, $conn);
+            }
+        }
+    }
+    mysqli_close($conn);
+}
+
 ?>
